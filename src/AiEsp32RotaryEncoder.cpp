fd6d2bdd7c001021655a6f75ca5dc01cae0c1279
Dorian <54947603+DorianFournier@users.noreply.github.com>
2023-02-02 02:14:14 +0100

    Update AiEsp32RotaryEncoder.cpp

--- src/AiEsp32RotaryEncoder.cpp
+++ src/AiEsp32RotaryEncoder.cpp
@@ -274,10 +274,9 @@ void AiEsp32RotaryEncoder::disable()
 bool AiEsp32RotaryEncoder::isEncoderButtonClicked(unsigned long maximumWaitMilliseconds)
 {
 	static bool wasTimeouted = false;
-	int button = 1 - digitalRead(encoderButtonPin);
-	//int button = digitalRead(encoderButtonPin);
+	//int button = 1 - digitalRead(encoderButtonPin);
+	int button = digitalRead(encoderButtonPin);
 	if (!button)
-	//if (button)
 	{
 		if (wasTimeouted)
 		{
@@ -287,10 +286,9 @@ bool AiEsp32RotaryEncoder::isEncoderButtonClicked(unsigned long maximumWaitMilli
 		return false;
 	}
 	delay(30); //debounce
-	button = 1 - digitalRead(encoderButtonPin);
-	//button = digitalRead(encoderButtonPin);
+	//button = 1 - digitalRead(encoderButtonPin);
+	button = digitalRead(encoderButtonPin);
 	if (!button)
-	//if (button)
 	{
 		return false;
 	}
@@ -298,8 +296,8 @@ bool AiEsp32RotaryEncoder::isEncoderButtonClicked(unsigned long maximumWaitMilli
 	//wait release of button but only maximumWaitMilliseconds
 	wasTimeouted = false;
 	unsigned long waitUntil = millis() + maximumWaitMilliseconds;
-	while (1 - digitalRead(encoderButtonPin))
-	//while (digitalRead(encoderButtonPin))
+	//while (1 - digitalRead(encoderButtonPin))
+	while (digitalRead(encoderButtonPin))
 	{
 		if (millis() > waitUntil)
 		{
